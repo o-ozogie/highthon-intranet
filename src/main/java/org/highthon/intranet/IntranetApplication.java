dfd42ecaca48270111afca774d6eebaf46620e08
Jeong woo yeong <wjd030811@gmail.com>
2021-12-08 22:30:23 +0900

    build: 스프링부트 프로젝트 셋업

---
+++ src/main/java/org/highthon/intranet/IntranetApplication.java
@@ -0,0 +1,13 @@
+package org.highthon.intranet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntranetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntranetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/highthon/intranet/IntranetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.highthon.intranet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntranetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
